a59cb47caaacadbc9330af0da0fbb507ca55a5e1
Vetrov-Anton <153396167+Vetrov-Anton@users.noreply.github.com>
2023-12-12 00:58:51 +0300

    Add files via upload

---
+++ count_aminoacid.py
@@ -0,0 +1,11 @@
+with open('faa.faa', mode='r') as f:
+    s = ''
+    for line in f:
+        x = line.strip()
+        if x[0] != '>':
+            s = s + x
+y = list(set(s))
+for i in y:
+    n = s.count(i)
+    z = n*100/len(s)
+    print(f'{i}\t{z:.02f}')
